2f39a7bc1cdedcbfee190c260e780206278cc017
Ayuri Bessa <ayuribessa@gmail.com>
2022-08-11 14:08:47 -0300

    Finaliza 1º parte do simulador

--- assets/js/financiamento.js
+++ assets/js/financiamento.js
@@ -1,6 +1,6 @@
 import { Parcela } from './parcela.js'
 
-class Financiamento {
+export class Financiamento {
     #taxaJuros; //juros mensais
     #prazo; //em meses
     #parcelas = [];
@@ -8,7 +8,35 @@ class Financiamento {
         this.#taxaJuros = taxaJuros;
         this.#prazo = prazo;
         this.#parcelas.push(new Parcela(0, 0, 0, 0, valor - entrada));
+    }
 
+    static calcJuros(valor, taxaJuros) {
+        return valor * (taxaJuros / 100);
+    }
+
+    calcParcelasMensais() {
+        let saldo = this.#parcelas[this.#parcelas.length - 1].getSaldo(); //saldo da última parcela
+        let prazo = this.#prazo - (this.#parcelas.length - 1);
+        let amortizacao = saldo / prazo;
+        for (let i = 0; i < prazo; i++) {
+            const numero = this.#parcelas.length;
+            const juros = Financiamento.calcJuros(saldo, this.#taxaJuros);
+            const valor = juros + amortizacao;
+            saldo -= amortizacao;
+            if (saldo < 0) { saldo = 0; }
+            this.#parcelas.push(new Parcela(numero, valor, juros, amortizacao, saldo));
+        }
+    }
+
+    exibeParcelas() {
+        const parcelas = this.#parcelas.slice(1); //pega a partir do elemento 1
+        for (const parcela of parcelas) {
+            const linha = corpoTabela.insertRow(-1); //-1 significa no final do elemento
+            for (const dado of parcela.getDadosFormatados()) {
+                const celula = linha.insertCell(-1);
+                celula.textContent = dado;
+            }
+        }
     }
 }
 
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,13 @@
+import { Financiamento } from "./financiamento.js";
+
 const comCarencia = document.querySelector('#comCarencia');
 const listaSuspensa = document.querySelector('#listSuspensa');
+const corpoTabela = document.querySelector('#corpoTabela');
+const botaCalcular = document.querySelector('#botaoCalcular');
+const textoValor = document.querySelector('#textoValor');
+const textoEntrada = document.querySelector('#textoEntrada');
+const textoTaxaJuros = document.querySelector('#textoTaxaJuros');
+const textoPrazo = document.querySelector('#textoPrazo');
 
 comCarencia.addEventListener('change', function () {
     if (this.checked) {
@@ -7,5 +15,15 @@ comCarencia.addEventListener('change', function () {
     } else {
         listaSuspensa.setAttribute('hidden', 'hidden');
     }
-
 })
+
+botaCalcular.addEventListener('click', function () {
+    const valor = parseFloat(textoValor.value);
+    const entrada = parseFloat(textoEntrada.value);
+    const taxaJuros = parseFloat(textoTaxaJuros.value);
+    const Prazo = parseFloat(textoPrazo.value);
+    let simulacao;
+    simulacao = new Financiamento(valor, entrada, taxaJuros, Prazo);
+    simulacao.calcParcelasMensais();
+    simulacao.exibeParcelas();
+});
